f7b4f2fe6f93c19e8a9c4d6cc2a319ca2cad99aa
rizwankhan <super047240@gmail.com>
2024-02-04 11:19:43 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routers/user.route.js'
+import authRouter from './routers/auth.route.js'
 dotenv.config()
 
 const app = express()
@@ -26,6 +27,7 @@ app.listen(3000, () => {
 })
 //mount router
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
 //get test router
 
 app.get('/test', (req, res) => {
